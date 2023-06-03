66cbe165a09508c0fdeef5577e35ea5f8edeb772
Kenny Baron <kennybaron@fastmail.com>
2023-06-02 23:40:53 -0400

    wip new gh repo remote

---
+++ README.md
@@ -0,0 +1,5 @@
+# Kenny's CLI Omnitool
+- dev and demo setup host and deploy for simple apps
+- project scaffolder and desktop/system config loader
+- systems statuses and notifications
+- repo cloner and forker
--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,113 @@
+use std::process::{Command, Stdio};
+
 use anyhow::{anyhow, Context, Result};
-use clap::Parser;
+use clap::{arg, Arg, ArgAction, Parser};
 
 #[derive(Parser)]
+#[command(arg_required_else_help = true)]
 pub struct Args {
     // #[arg(short = 'o', long = "output")]
     pattern: String,
     path: std::path::PathBuf,
 }
 
-#[derive(Debug)]
-struct CustomError(String);
-
 pub fn run() -> Result<()> {
-    let args = Args::parse();
+    // let args = Args::parse();
+    cli();
 
-    if args.pattern.is_empty() {
-        return Err(anyhow!("pattern cannot be empty"));
+    // gh_new_remote_repo()?;
+
+    // String::from_utf8(output.stdout)?
+    //     .lines()
+    //     .for_each(|x| println!("{}", x));
+
+    // if args.pattern.is_empty() {
+    //     return Err(anyhow!("pattern cannot be empty"));
+    // }
+
+    // let content = std::fs::read_to_string(&args.path)
+    //     .with_context(|| format!("could not read file `{}`", &args.path.display()))?;
+
+    // find_matches(&content, &args.pattern, &mut std::io::stdout())?;
+
+    Ok(())
+}
+
+fn gh_create_remote_repo(name: &str, is_public: bool) -> Result<()> {
+    let privacy = if is_public { "--public" } else { "--private" };
+
+    println!("gh fn recvd: name {} public flag {}", name, privacy);
+    // let status = Command::new("/usr/bin/gh")
+    //     .arg("repo")
+    //     .arg("create")
+    //     .arg(name)
+    //     .arg(privacy)
+    //     .status()
+    //     .context("Failed to execute 'gh repo create'")?; // for OS level errors
+
+    // // check success of command exec itself
+    // if !status.success() {
+    //     return Err(anyhow!(
+    //         "'gh repo create' failed with exit status {}",
+    //         status
+    //     ));
+    // }
+
+    // let url = format!("https://github.com/protofarer/{}", name);
+    // let status = Command::new("/usr/bin/gh")
+    //     .arg("remote")
+    //     .arg("add")
+    //     .arg("add")
+    //     .arg("origin")
+    //     .arg(&url)
+    //     .status()
+    //     .context("Failed to execute 'git remote add'")?;
+
+    // if !status.success() {
+    //     return Err(anyhow!(
+    //         "'git remote add' failed with exit status {}",
+    //         status
+    //     ));
+    // }
+
+    Ok(())
+}
+
+fn cli() -> Result<()> {
+    let matches = clap::Command::new("kli")
+        .version("0.1.0")
+        .author("Kenny <kennybaron@fastmail.com")
+        .about("Kenny's CLI Omnitool")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .allow_external_subcommands(true)
+        .subcommand(
+            clap::Command::new("repo")
+                .about("Creates a new GitHub repo, private by default")
+                .arg(arg!([NAME]))
+                .arg_required_else_help(true)
+                .about("Set repo name")
+                // .required(true)
+                .arg(
+                    Arg::new("public")
+                        .long("public")
+                        .short('p')
+                        .action(ArgAction::SetTrue),
+                )
+                .about("Makes a public repo"),
+        )
+        .get_matches();
+    match matches.subcommand() {
+        Some(("repo", sub_matches)) => {
+            let name = sub_matches.get_one::<String>("NAME").unwrap();
+            let is_public = sub_matches.get_flag("public");
+            gh_create_remote_repo(name, is_public)?;
+        }
+        _ => unreachable!("Exhausted list of subcommands, subcomman_required prevents `None`"),
     }
-
-    // ! main doesn't see the read_to_string level error
-    let content = std::fs::read_to_string(&args.path)
-        .with_context(|| format!("could not read file `{}`", &args.path.display()))?;
-
-    find_matches(&content, &args.pattern, &mut std::io::stdout())?;
-
+    // let name = matches.value_of("name").unwrap();
+    // let is_public = matches.is_present("public");
+    // run_create_repo(name, is_public)?;
     Ok(())
 }
 
