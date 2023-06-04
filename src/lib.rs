c8f051a70d91e55b7de32fcc1e0cc6f2042e5729
Kenny Baron <kennybaron@fastmail.com>
2023-06-03 22:20:31 -0400

    Fin: create and delete repo commands

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,96 @@
+#![allow(warnings)]
+
 use std::process::{Command, Stdio};
 
 use anyhow::{anyhow, Context, Result};
 use clap::{arg, command, Arg, ArgAction};
 
-pub fn gh_create_remote_repo(name: &str, is_public: bool) -> Result<()> {
-    println!("gh fn recvd: name {} public flag {}", name, is_public);
-    // let status = Command::new("/usr/bin/gh")
-    //     .arg("repo")
-    //     .arg("create")
-    //     .arg(name)
-    //     .arg(privacy)
-    //     .status()
-    //     .context("Failed to execute 'gh repo create'")?; // for OS level errors
+pub fn gh_create_repo(name: &str, is_public: bool) -> Result<()> {
+    // cwd must have a git repo
+    let output = Command::new("/usr/bin/git")
+        .arg("rev-parse")
+        .arg("--is-inside-work-tree")
+        .output()
+        .context("Failed to execute 'git rev-parse --is-inside-work-tree")?;
 
-    // // check success of command exec itself
-    // if !status.success() {
-    //     return Err(anyhow!(
-    //         "'gh repo create' failed with exit status {}",
-    //         status
-    //     ));
-    // }
+    if !output.status.success() {
+        return Err(anyhow!(
+            "Error: Cannot create remote repo without a local repo in cwd"
+        ));
+    }
 
-    // let url = format!("https://github.com/protofarer/{}", name);
-    // let status = Command::new("/usr/bin/gh")
-    //     .arg("remote")
-    //     .arg("add")
-    //     .arg("add")
-    //     .arg("origin")
-    //     .arg(&url)
-    //     .status()
-    //     .context("Failed to execute 'git remote add'")?;
+    let status = Command::new("/usr/bin/gh")
+        .arg("repo")
+        .arg("create")
+        .arg(name)
+        .arg(if is_public { "--public" } else { "--private" })
+        .status()
+        .context("Failed to execute 'gh repo create'")?;
 
-    // if !status.success() {
-    //     return Err(anyhow!(
-    //         "'git remote add' failed with exit status {}",
-    //         status
-    //     ));
-    // }
+    if !status.success() {
+        return Err(anyhow!(
+            "'gh repo create' failed with exit status {}",
+            status
+        ));
+    }
+
+    let url = format!("https://github.com/protofarer/{}", name);
+    let status = Command::new("/usr/bin/git")
+        .arg("remote")
+        .arg("add")
+        .arg("origin")
+        .arg(&url)
+        .status()
+        .context("Failed to execute 'git remote add'")?;
+
+    if !status.success() {
+        return Err(anyhow!(
+            "'git remote add' failed with exit status {}",
+            status
+        ));
+    }
+
+    let status = Command::new("/usr/bin/git")
+        .arg("push")
+        .arg("-u")
+        .arg("origin")
+        .arg("main")
+        .status()
+        .context("Failed to execute 'git push -u origin main'")?;
+
+    if !status.success() {
+        return Err(anyhow!(
+            "'git push -u origin main' failed with exit status {}",
+            status
+        ));
+    }
+
+    Ok(())
+}
+
+pub fn gh_remove_repo(name: &str, yes: bool) -> Result<()> {
+    let status = match yes {
+        true => Command::new("/usr/bin/gh")
+            .arg("repo")
+            .arg("delete")
+            .arg("https://github.com/protofarer/".to_owned() + name)
+            .arg("--yes")
+            .status()
+            .context("Failed to execute 'gh repo delete --yes'")?,
+        false => Command::new("/usr/bin/gh")
+            .arg("repo")
+            .arg("delete")
+            .arg("https://github.com/protofarer/".to_owned() + name)
+            .status()
+            .context("Failed to execute 'gh repo delete'")?,
+    };
+
+    if !status.success() {
+        return Err(anyhow!(
+            "'gh repo delete' failed with exit status {}",
+            status
+        ));
+    }
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![allow(warnings)]
 
 use clap::{command, Args, Parser, Subcommand};
-use kli::gh_create_remote_repo;
+use kli::{gh_create_repo, gh_remove_repo};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -14,30 +14,63 @@ struct Cli {
 enum Commands {
     #[command(subcommand)]
     New(NewCommands),
+
+    #[command(subcommand)]
+    Rem(RemCommands),
 }
 
 #[derive(Subcommand)]
 enum NewCommands {
-    Repo(NewRepoArgs),
+    Rep(NewRepArgs),
     Web { name: String },
 }
 
 #[derive(Args)]
-struct NewRepoArgs {
+struct NewRepArgs {
     name: String,
     #[arg(short, long)]
     public: bool,
 }
 
+#[derive(Subcommand)]
+enum RemCommands {
+    Rep(RemRepArgs),
+}
+
+#[derive(Args)]
+struct RemRepArgs {
+    repo: String,
+    #[arg(short, long)]
+    yes: bool,
+}
+
 fn main() {
     let cli = Cli::parse();
     match &cli.command {
-        Commands::New(NewCommands::Repo(NewRepoArgs { name, public })) => {
-            gh_create_remote_repo(name, *public);
+        Commands::New(NewCommands::Rep(NewRepArgs { name, public })) => {
+            main_handle_result(gh_create_repo(name, *public));
+            // match gh_create_repo(name, *public) {
+            //     Ok(v) => println!("success"),
+            //     Err(e) => eprintln!("{:?}", e),
+            // }
         }
+        Commands::Rem(RemCommands::Rep(RemRepArgs { repo, yes })) => {
+            // match gh_remove_repo(repo, *prompt) {
+            //     Ok(v) => println!("success"),
+            //     Err(e) => eprintln!("{:?}", e),
+            // }
+            main_handle_result(gh_remove_repo(repo, *yes));
+        }
+
         Commands::New(NewCommands::Web { name }) => {
             println!("{name}",)
         }
         _ => unreachable!("no commands heere"),
+    }
+}
+
+fn main_handle_result(result: anyhow::Result<()>) {
+    if let Err(e) = result {
+        eprintln!("{:?}", e);
     }
 }
